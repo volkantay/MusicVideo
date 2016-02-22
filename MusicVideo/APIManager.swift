e149f9d4cc976cba6a60cc63f54bcc3c77e6457b
Volkan Tay <volkan@volkan-mbp.zyxel.com>
2016-02-22 02:11:54 +0200

    part-3 completed

--- MusicVideo/APIManager.swift
+++ MusicVideo/APIManager.swift
@@ -19,22 +19,34 @@ class APIManager {
         
         let url = NSURL(string: urlString)!
         
-        let task = session.dataTaskWithURL(url) { (data, response, error) -> Void in
-          
-                dispatch_async(dispatch_get_main_queue())
-                    {
-                    
-                      if error != nil {
-                    completion(result: (error!.localizedDescription))
-                   }
-                   else {
-                         completion(result: "İşlem Başarılı")
-                 print(data)
-                          }
         
+        let task = session.dataTaskWithURL(url) { (data, response, error) -> Void in
+            if error != nil {
+                dispatch_async(dispatch_get_main_queue()) {
+                    completion(result: (error!.localizedDescription))
+                }
+            } else {
+                // json serilization ekleniyor
+                do {
+                    if let json = try NSJSONSerialization.JSONObjectWithData(data!, options: .AllowFragments) as? JSONDictionary {
+                        print(json)
+                        let priority = DISPATCH_QUEUE_PRIORITY_HIGH
+                        dispatch_async(dispatch_get_global_queue(priority, 0)) {
+                            dispatch_async(dispatch_get_main_queue()) {
+                                completion(result: "JSONSerialization Successful")
+                            }
+                        }
+                    }
+                } catch {
+                    dispatch_async(dispatch_get_main_queue()) {
+                        completion(result: "Error in NSJSONSerialization")
+                    }
+                    
+                }
             }
         }
         task.resume()
-        
+    
+    
     }
 }
